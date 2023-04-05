60b47ea7b53c5fe9b4299ef2a2519a16fb963fa3
HP-PC <HP-PC@LAPTOP-9M63BUFU>
2023-04-06 03:16:56 +0530

    This is my second comit

---
+++ Examples/src/Example/Sample_example2.java
@@ -0,0 +1,15 @@
+package Example;
+
+public class Sample_example2 {
+	
+	void method1()
+	{
+		System.out.println(" Second exapmle");
+}
+
+public static void main(String args[])
+{
+	Sample_example2 obj1=new Sample_example2();
+	obj1.method1();
+}
+}
