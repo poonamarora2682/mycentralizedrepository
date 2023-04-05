789b1cd8ec799f4684a675afe56b2b65bd14e7b7
HP-PC <HP-PC@LAPTOP-9M63BUFU>
2023-04-06 03:03:11 +0530

    Pushing the code first time

---
+++ Examples/src/Example/Sample_example.java
@@ -0,0 +1,15 @@
+package Example;
+
+public class Sample_example 
+{
+
+	public void method1()
+	{
+		System.out.println("This is my first java program");
+	}
+	public static void main(String[] args)
+	{
+		Sample_example obj1=new Sample_example();
+		obj1.method1();
+	}
+}
